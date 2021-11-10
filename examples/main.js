6b596047c0596de17af7063559cda5227d8ab806
lanteng <lan.teng@qq.com>
2021-11-10 17:16:58 +0800

    build: remove package-lock and examples

--- examples/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VGraphin from "../src/index";
-Vue.use(VGraphin);
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
